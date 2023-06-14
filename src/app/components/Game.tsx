1054e65d1fa7725982b38a5cdb374d70b681bbe2
rubi <rubi0624@nate.com>
2023-06-14 18:14:31 +0900

    last commit

--- src/app/components/Game.tsx
+++ src/app/components/Game.tsx
@@ -3,14 +3,16 @@ import Board from "./Board";
 import { Square } from "./types";
 
 const Game = () => {
-  const [xIsNext, setXIsNext] = useState<boolean>(true);
   const [history, setHistory] = useState<Square[][]>([Array(9).fill(null)]); // 이중배열
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState<number>(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
   // 배열의 마지막꺼만 나오게 (최신)
 
   const handlePlay = (nextSquares: Square[]) => {
-    setHistory([...history, nextSquares]); // 이중배열 까줌 ...
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory); // 이중배열 까줌 ...
+    setCurrentMove(nextHistory.length - 1);
 
     // [1, 2, 3].push(1); // [1,2,3,1]
 
@@ -18,16 +20,31 @@ const Game = () => {
     console.log(history);
   };
 
-  const jumpTo = () => {
-    // todo
+  const jumpTo = (nextMove: number) => {
+    setCurrentMove(nextMove);
   };
+
+  const moves = history.map((squares, move: number) => {
+    let description;
+    if (move > 0) {
+      description = "go to move #" + move;
+    } else {
+      description = "go to game start";
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
-      <div className="game-info"></div>
+      <div className="game-info">{moves}</div>
     </div>
   );
 };
